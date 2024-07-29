96ce8ffa5042d3070a0c4f5003cfe65f7269c28a
FabianDom <fabiandmz13@gmail.com>
2024-07-28 22:34:22 -0500

    funciones

--- app.js
+++ app.js
@@ -6,3 +6,34 @@ o: "ober",
 u: "ufat",
 
 };
+
+let llavesDesencriptacion = {
+    ai: "a",
+    enter: "e",
+    imes: "i",
+    ober: "o",
+    ufat: "u",
+    
+    };
+    
+
+
+
+
+function encriptarTexto () {
+    let textoUsuario = document.getElementById('texto_encriptador').value;
+    let textoUsuarioEncriptado = '';
+    console.log(textoUsuario)
+    for (let i = 0; i < textoUsuario.length; i++) {
+        let caracterObtenido = textoUsuario[i]
+        if(llavesEncriptacion[caracterObtenido]) {
+        textoUsuarioEncriptado += llavesEncriptacion[caracterObtenido]
+        } else {
+            textoUsuarioEncriptado += caracterObtenido
+        }
+    }
+    let textoEncriptadoFinal = document.getElementById('texto-encriptado')
+    textoEncriptadoFinal.innerHTML = textoUsuarioEncriptado
+    
+};
+
