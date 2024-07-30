0407af16a8b0e809afe5e6d80b0001fda887a0a6
FabianDom <fabiandmz13@gmail.com>
2024-07-29 21:57:39 -0500

    Funcion descriptador

--- app.js
+++ app.js
@@ -1,39 +1,81 @@
 let llavesEncriptacion = {
-a: "ai",
-e: "enter",
-i: "imes",
-o: "ober",
-u: "ufat",
-
+  a: "ai",
+  e: "enter",
+  i: "imes",
+  o: "ober",
+  u: "ufat",
 };
 
 let llavesDesencriptacion = {
-    ai: "a",
-    enter: "e",
-    imes: "i",
-    ober: "o",
-    ufat: "u",
-    
-    };
-    
-
-
-
-
-function encriptarTexto () {
-    let textoUsuario = document.getElementById('texto_encriptador').value;
-    let textoUsuarioEncriptado = '';
-    console.log(textoUsuario)
-    for (let i = 0; i < textoUsuario.length; i++) {
-        let caracterObtenido = textoUsuario[i]
-        if(llavesEncriptacion[caracterObtenido]) {
-        textoUsuarioEncriptado += llavesEncriptacion[caracterObtenido]
-        } else {
-            textoUsuarioEncriptado += caracterObtenido
-        }
-    }
-    let textoEncriptadoFinal = document.getElementById('texto-encriptado')
-    textoEncriptadoFinal.innerHTML = textoUsuarioEncriptado
-    
+  ai: "a",
+  enter: "e",
+  imes: "i",
+  ober: "o",
+  ufat: "u",
 };
 
+function encriptarTexto() {
+  // Obtener el texto ingresado por el usuario desde el campo de entrada
+  let textoUsuario = document.getElementById("texto_encriptador").value;
+  let textoUsuarioEncriptado = ""; // Variable para almacenar el texto encriptado
+
+  // Iterar sobre cada carácter del texto ingresado
+  for (let i = 0; i < textoUsuario.length; i++) {
+    let caracterObtenido = textoUsuario[i]; // Obtener el carácter actual
+
+    // Verificar si el carácter tiene una correspondencia en llavesEncriptacion
+    if (llavesEncriptacion[caracterObtenido]) {
+      // Si hay correspondencia, añadir el valor encriptado correspondiente
+      textoUsuarioEncriptado += llavesEncriptacion[caracterObtenido];
+    } else {
+      // Si no hay correspondencia, añadir el carácter tal como está
+      textoUsuarioEncriptado += caracterObtenido;
+    }
+  }
+
+  // Mostrar el texto encriptado.
+  let textoEncriptadoFinal = document.getElementById("texto-encriptado");
+  textoEncriptadoFinal.innerHTML = textoUsuarioEncriptado;
+// verificar si el texto se encuentra encriptado
+  for (const propiedad in llavesDesencriptacion) {
+    if (textoUsuario.includes(propiedad)) {
+      let textoUsuarioFinal = document.getElementById("texto-encriptado");
+      textoUsuarioFinal.innerHTML = "El texto esta encriptado";
+      
+    }
+  }
+}
+
+function desencriptarTexto() {
+  // Obtener el texto ingresado por el usuario desde el campo de entrada
+  let textoUsuario = document.getElementById("texto_encriptador").value;
+  let textoUsuarioDesencriptado = textoUsuario; // Variable para almacenar el texto desencriptado
+
+  // Iterar sobre cada propiedad en llavesDesencriptacion
+  for (const propiedad in llavesDesencriptacion) {
+    let valorLlaveDesencriptada = llavesDesencriptacion[propiedad]; // Obtener el valor de las llaves de desencriptación
+    let coincidencias = new RegExp(propiedad, "g"); // Crear una expresión regular para encontrar todas las coincidencias de la llaves en el texto.
+
+    // Reemplazar todas las coincidencias de la clave en el texto por el valor de desencriptación
+    textoUsuarioDesencriptado = textoUsuarioDesencriptado.replace(
+      coincidencias,
+      valorLlaveDesencriptada
+    );
+  }
+
+  // Mostrar el texto desencriptado en el elemento HTML con id 'texto-encriptado'
+  let textoDesencriptadoFinal = document.getElementById("texto-encriptado");
+  textoDesencriptadoFinal.innerHTML = textoUsuarioDesencriptado;
+}
+
+function limpiarCampoTextoEncriptado() {
+  let resultadoTexto = document.getElementById("texto_encriptador").value;
+  if (resultadoTexto.trim() === "") {
+    let limpiarTexto = document.getElementById("texto-encriptado");
+    limpiarTexto.innerHTML = "";
+  }
+}
+
+document
+  .getElementById("texto_encriptador")
+  .addEventListener("input", limpiarCampoTextoEncriptado);
